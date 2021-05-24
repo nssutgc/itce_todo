9c9f2e7f5f9979a7d10fab5490b401143d6cd4a9
Takahiro TOGASHI <togac43@gmail.com>
2021-05-24 15:43:19 +0900

    ルートの更新

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,11 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('tasks');
-});
+    return view('tasks', [
+        'tasks' => App\Task::latest()->get()
+    ]);
+ });
+
 Route::post('/task', function (Request $request) {
-    //
-});
+    request()->validate(
+        [
+            'name' => 'required|unique:tasks|min:3|max:255'
+        ],
+        [
+            'name.required' => 'タスク内容を入力してください。',
+            'name.unique' => 'そのタスクは既に追加されています。',
+            'name.min' => '3文字以上で入力してください。',
+            'name.max' => '255文字以内で入力してください。'
+        ]
+    );
+    $task = new Task();
+    $task->name = request('name');
+    $task->save();
+    return redirect('/');
+ });
+
 Route::delete('/task/{task}', function () {
     //
 });
