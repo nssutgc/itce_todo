0d9c9a3fb1d48d237faedc4b6d65467ecdbd5e46
Takahiro TOGASHI <togac43@gmail.com>
2021-05-24 14:41:46 +0900

    ルートの設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+Route::post('/task', function (Request $request) {
+    //
+});
+Route::delete('/task/{task}', function () {
+    //
 });
